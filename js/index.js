4764d5ced2cfb5be3a9ee578a0e9d5d67c79d9ee
can2 <279319529@qq.com>
2018-02-04 16:01:17 +0800

    更新图片

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// var name = null;
+$('#next').on('click', function() {
+  if($('#name').val() == ''){
+    alert('请输入姓名');
+    return;
+  }
+  $('.t span').text($('#name').val());
+  $.fn.fullpage.moveNext(true);
+})

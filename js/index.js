2c0a47b0cc2406121961910cb0576ace2867535d
can2 <279319529@qq.com>
2018-02-05 13:19:03 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 // var name = null;
-
+var num = Math.floor(Math.random() * 13);
+$('.item-2 .qian img').attr('src', '/img/qian/'+ num +'.png');
 $('#next').on('click', function() {
   if($('#name').val() == ''){
     alert('请输入姓名');
     return;
   }
   $('.t span').text($('#name').val());
-  var num = Math.floor(Math.random() * 13);
-  $('.item-2 .qian img').attr('src', '/img/qian/'+ num +'.png');
+  
+  
   $.fn.fullpage.moveNext(true);
 })

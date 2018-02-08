3a620a339780fda9ab46569939e2a9e76a946c00
can2 <279319529@qq.com>
2018-02-08 17:41:34 +0800

    fix

--- ser.js
+++ ser.js
@@ -56,3 +56,43 @@ function getMIME(extname) {
       }
       
 }
+
+const getAccessToken = function () {
+  let queryParams = {
+    'grant_type': 'client_credential',
+    'appid': config.appId,
+    'secret': config.appSecret
+  };
+
+  let wxGetAccessTokenBaseUrl = 'https://api.weixin.qq.com/cgi-bin/token?'+qs.stringify(queryParams);
+  let options = {
+    method: 'GET',
+    url: wxGetAccessTokenBaseUrl
+  };
+  return new Promise((resolve, reject) => {
+    request(options, function (err, res, body) {
+      if (res) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(err);
+      }
+    });
+  })
+};
+
+//保存与更新
+const saveToken = function () {
+  getAccessToken().then(res => {
+    let token = res['access_token'];
+    fs.writeFile('./token', token, function (err) {
+      
+    });
+  })
+};
+
+const refreshToken = function () {
+  saveToken();
+  setInterval(function () {
+    saveToken();
+  }, 7000*1000);
+};

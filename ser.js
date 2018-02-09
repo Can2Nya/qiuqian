cf8847ea1b0930d02155184ff9ea2191b14b381e
can2 <279319529@qq.com>
2018-02-09 22:43:07 +0800

    fix

--- ser.js
+++ ser.js
@@ -25,6 +25,7 @@ const getAccessToken = function () {
   return new Promise((resolve, reject) => {
     request(options, function (err, res, body) {
       if (res) {
+        console.log(body);
         resolve(JSON.parse(body));
       } else {
         reject(err);
@@ -37,7 +38,7 @@ const getAccessToken = function () {
 const saveToken = function () {
   getAccessToken().then(res => {
     let token = res['access_token'];
-    fs.writeFile('./token', token, function (err) {
+    fs.writeFile('./token.txt', token, function (err) {
       console.log('accessToken 保存成功');
     });
   })
@@ -60,55 +61,55 @@ function start(){
 
 start();
 
-http.createServer(function(req, res) {
-    //得到用户的路径
-    var pathname = url.parse(req.url).pathname;
-    if(pathname == "/") {
-        pathname = "index.html";
-    }
-    //拓展名
-    var extname = path.extname(pathname);
-    // console.log(extname);
-    //真的读取这个文件
-    //缺点，太智能，但是不能检测是什么类型的文件
-    fs.readFile("./" + pathname, function(err, data) {
-      console.log(pathname);
-        //data是一个Buffer，二进制的数据流
-        if(err) {
-            //如果文件不存在，就返回404
-            fs.readFile("./static/404.html", function(err, data) {
-                res.writeHead(404,{"Content-type":"text/html;chaset=UTF8"});
-                res.end(data);
-            });
-            return ;//必须这个，不然会走到下个data
-        }
-        //MIME类型，就是
-        //网页文件：text/html
-        //jpg文件：image/jpg
-        var  mime = getMIME(extname)
-        res.writeHead(200,{"Content-type":mime});
-        res.end(data);
-    })
-}).listen(80, "0.0.0.0");
-console.log('listen 80')
+// http.createServer(function(req, res) {
+//     //得到用户的路径
+//     var pathname = url.parse(req.url).pathname;
+//     if(pathname == "/") {
+//         pathname = "index.html";
+//     }
+//     //拓展名
+//     var extname = path.extname(pathname);
+//     // console.log(extname);
+//     //真的读取这个文件
+//     //缺点，太智能，但是不能检测是什么类型的文件
+//     fs.readFile("./" + pathname, function(err, data) {
+//       console.log(pathname);
+//         //data是一个Buffer，二进制的数据流
+//         if(err) {
+//             //如果文件不存在，就返回404
+//             fs.readFile("./static/404.html", function(err, data) {
+//                 res.writeHead(404,{"Content-type":"text/html;chaset=UTF8"});
+//                 res.end(data);
+//             });
+//             return ;//必须这个，不然会走到下个data
+//         }
+//         //MIME类型，就是
+//         //网页文件：text/html
+//         //jpg文件：image/jpg
+//         var  mime = getMIME(extname)
+//         res.writeHead(200,{"Content-type":mime});
+//         res.end(data);
+//     })
+// }).listen(80, "0.0.0.0");
+// console.log('listen 80')
 
-function getMIME(extname) {
-  switch (extname) {
-      case ".html":
-          return "text/html";
-          break;
-      case ".jpg":
-          return "image/jpg";
-          break;
-      case ".png":
-          return "image/png";
-          break;
-      case ".css":
-          return "text/css";
-          break;
-      case ".js":
-          return "application/x-javascript";
-          break;
-      }
+// function getMIME(extname) {
+//   switch (extname) {
+//       case ".html":
+//           return "text/html";
+//           break;
+//       case ".jpg":
+//           return "image/jpg";
+//           break;
+//       case ".png":
+//           return "image/png";
+//           break;
+//       case ".css":
+//           return "text/css";
+//           break;
+//       case ".js":
+//           return "application/x-javascript";
+//           break;
+//       }
       
-}
+// }

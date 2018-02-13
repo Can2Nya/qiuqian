132348b856ea6324033cf0289d2a6f224c729f9a
can2 <279319529@qq.com>
2018-02-13 11:16:48 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -116,7 +116,7 @@
 
 })(window, window['lib'] || (window['lib'] = {}));
 
-var num = Math.floor(Math.random() * 12);
+var num = Math.floor(Math.random() * 11);
 // $('.item-2 .qian').css('background-image', 'url(/img/qian/'+ num +'.png)');
 $('#drow').attr('src', '/img/qian/'+ num +'.png');
 $('#next').on('click', function() {

c59501f45bcc78a41f68b42572cd3212673bc3ba
can2 <279319529@qq.com>
2018-02-14 15:48:43 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -115,17 +115,3 @@
   }
 
 })(window, window['lib'] || (window['lib'] = {}));
-
-var num = Math.floor(Math.random() * 11);
-// $('.item-2 .qian').css('background-image', 'url(/img/qian/'+ num +'.png)');
-$('#drow').attr('src', '/img/qian/'+ num +'.png');
-$('#next').on('click', function() {
-  if($('#name').val() == ''){
-    alert('请输入姓名');
-    return;
-  }
-  $('.t span').text($('#name').val());
-  
-  
-  $.fn.fullpage.moveNext();
-})
